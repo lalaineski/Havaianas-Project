2ca0def5e108a5d40259787d59aa98a522942863
katalina1212 <53251633+katalina1212@users.noreply.github.com>
2019-10-02 11:42:31 +0100

    wishlist

---
+++ list.php
@@ -0,0 +1,30 @@
+<?php
+require('vendor/autoload.php');
+//get user's wishlist total
+use havaianas\WishList;
+$wish = new WishList();
+if( $_SERVER['REQUEST_METHOD'] == 'GET' && $_GET['action'] == 'delete'){
+  $product_id = $_GET['product_id'];
+  $delete = $wish -> removeItem( $product_id );
+}
+$wish_total = $wish -> getWishListTotal();
+$wish_items = $wish -> getWishListItems();
+// create navigation
+use havaianas\Navigation;
+$nav = new Navigation();
+$navigation = $nav -> getNavigation();
+//create twig loader for templates
+$loader = new Twig_Loader_Filesystem('templates');
+//create twig environment and pass the loader
+$twig = new Twig_Environment($loader);
+//call a twig template
+$template = $twig -> load('wishlist.twig');
+//output the template and pass the data
+echo $template -> render( array(
+  'result' => $result,
+  'navigation' => $navigation,
+  'wish' => $wish_total,
+  'wish_items' => $wish_items,
+  'title' => "Wish List"
+) );
+?>
